2ed0448aa1cbf779883c72d8d92eb2f140ad19c4
safaat <muhsafaat21@gmail.com>
2025-12-13 02:08:34 +0800

    warna

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
             </style>
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="bg-[#FDFDFC] dark:bg-[#1b1b] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         {{-- Auth header removed; auth buttons are placed inside the welcome box below the text --}}
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow">
             <main class="flex max-w-[335px] w-2/4 flex-col-reverse lg:max-w-4xl lg:flex-row">
