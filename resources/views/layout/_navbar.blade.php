7122dd2df7a877ad0cf6b55c82fca68fd0a74d62
safaat <muhsafaat21@gmail.com>
2025-12-14 23:05:16 +0800

    update

--- resources/views/layout/_navbar.blade.php
+++ resources/views/layout/_navbar.blade.php
@@ -3,23 +3,9 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-    </ul>
-    <ul>
         <li>
-            <div class="image">
             <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
             <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-            </div>
         </li>
-        {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-            <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-            </div>
-        </div> --}}
     </ul>
 </nav>
