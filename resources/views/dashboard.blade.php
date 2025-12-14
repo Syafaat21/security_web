6c0543610e7a2ed6edc8bae5d917e5f349caa4a7
safaat <muhsafaat21@gmail.com>
2025-12-14 22:04:40 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,36 +2,16 @@
 
 @section('content')
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
         <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Customer Dashboard</h1>
-                        <p>Welcome to the customer dashboard!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Customer Menu</h3>
-                            </div>
-                            <div class="card-body">
-                                <ul>
-                                    <li><a href="/customer/special">Special Customer Menu</a></li>
-                                    <li><a href="/customer/recommendation">Recommended Feature</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+                <p>Ini Halaman Dashboard</p>
+            </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
         </div>
     </div>
 @endsection
