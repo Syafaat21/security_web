91db0809d8b93812eea8cee8c2bbb8c4a653cf00
safaat <muhsafaat21@gmail.com>
2025-12-15 00:15:38 +0800

    update

--- resources/views/menu/user.blade.php
+++ resources/views/menu/user.blade.php
@@ -1,29 +1,16 @@
 @extends('layout.master')
 
-@section('title', 'User Management')
-
 @section('content')
-<div class="container mt-4">
-    <h1>User Management</h1>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($users as $user)
-            <tr>
-                <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->role }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <div class="content-wrapper">
+        <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Users</h1>
+                <p>Ini Halaman Users</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
 @endsection
