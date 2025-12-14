f9effa4a401751937c8f701495dd7adcc3e07b2f
safaat <muhsafaat21@gmail.com>
2025-12-15 02:20:53 +0800

    update

--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -53,14 +53,14 @@
                 {{ session('success') }}
                 </div>
 
-                @if(session('reset_token'))
+                {{-- @if(session('reset_token'))
                     <small>
                         <strong>Link Reset Password untuk Testing:</strong><br>
                         <a href="{{ url('/reset_password/' . session('reset_token')) }}" target="_blank" class="alert-link">
                             {{ url('/reset_password/' . session('reset_token')) }}
                         </a>
                     </small>
-                @endif
+                @endif --}}
             @endif
             <div class="input-group mb-3">
                 <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}">
