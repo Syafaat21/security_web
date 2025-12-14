9e5ef785411cf5879866598f7fb831d76aaba3e8
safaat <muhsafaat21@gmail.com>
2025-12-14 22:53:37 +0800

    update

--- resources/views/menu/user.blade.php
+++ resources/views/menu/user.blade.php
@@ -2,16 +2,15 @@
 
 @section('content')
     <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
         <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0">Users</h1>
                 <p>Ini Halaman Users</p>
-            </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+            </div>
+            </div>
+        </div>
         </div>
     </div>
 @endsection
