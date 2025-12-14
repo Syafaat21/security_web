97630988aea2757b6d70ca5aec5270630900f95f
safaat <muhsafaat21@gmail.com>
2025-12-14 23:07:50 +0800

    update

--- resources/views/layout/_navbar.blade.php
+++ resources/views/layout/_navbar.blade.php
@@ -3,9 +3,9 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li>
-            <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+        <li class="nav-item">
+            <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2 mt-3 pb-3 mb-3 d-flex" alt="User Image">
+            <a href="#" class="d-block info">{{ auth()->user()->name }}</a>
         </li>
     </ul>
 </nav>
