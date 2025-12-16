a08c544fc86bed0a374772daf196ab744be014c0
safaat <muhsafaat21@gmail.com>
2025-12-16 22:24:57 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,15 +2,85 @@
 
 @section('content')
     <div class="content-wrapper">
-        <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Dashboard</h1>
-                <p>Ini Halaman Dashboard</p>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row mt-3">
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ auth()->user()->name }}</h3>
+                                <p>Welcome Back!</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>Verified</h3>
+                                <p>Account Status</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-checkmark"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>0</h3>
+                                <p>Pending Tasks</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-clipboard"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>0</h3>
+                                <p>Notifications</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-android-notifications"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Recent Activities</h3>
+                            </div>
+                            <div class="card-body">
+                                <p>No recent activities.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Account Information</h3>
+                            </div>
+                            <div class="card-body">
+                                <p><strong>Name:</strong> {{ auth()->user()->name }}</p>
+                                <p><strong>Email:</strong> {{ auth()->user()->email }}</p>
+                                <p><strong>Role:</strong> {{ auth()->user()->role }}</p>
+                                <p><strong>Status:</strong> {{ auth()->user()->status }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
-        </div>
+        </section>
     </div>
 @endsection
