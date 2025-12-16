1aa56f6f9c9956c0dc21bb3d4a9c87548bd1e8ee
safaat <muhsafaat21@gmail.com>
2025-12-16 21:46:36 +0800

    update

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
     <div class="content-wrapper">
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Customer Dashboard</h1>
-                        <p>Selamat datang di dashboard pelanggan!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
