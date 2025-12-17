636f335ae9ea8160796218cd5c2b47eb4d38daa9
safaat <muhsafaat21@gmail.com>
2025-12-17 21:09:45 +0800

    anjay

--- app/Models/verification.php
+++ app/Models/verification.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class verification extends Model
+class Verification extends Model
 {
     use HasFactory;
     protected $guarded = [];
